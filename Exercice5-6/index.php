34adde11dc3d2a4c77d9ac1854d27f9991aa0789
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-05 16:26:03 +0100

    Exercice 5 et 6

---
+++ Exercice5-6/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 5</title>
+</head>
+<body>
+
+    <?php 
+    
+    if (isset($_POST['gender'], $_POST['firstName'], $_POST['lastName']) && (!empty($_POST['gender']) && !empty($_POST['firstName']) && !empty($_POST['lastName'])) ) {
+        var_dump($_POST);
+    }else {
+        ?>
+        <form action="./index.php" method="post">
+        <select name="gender" id="gender">
+            <option value="Mr">Mr</option>
+            <option value="Mme">Mme</option>
+        </select>
+
+        <label for="firstName">Prénom :</label>
+        <input type="text" name="firstName" id="firstName">
+
+        <label for="lastName">Nom :</label>
+        <input type="text" name="lastName" id="lastName">
+        
+        <input type="submit" value="submit">
+    </form>
+    <?php
+    }
+    ?>
+
+    
+</body>
+</html>
