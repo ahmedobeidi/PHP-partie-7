790639384c34b490542bd47bdf655be7dc5d12ee
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-05 16:33:19 +0100

    DONE: Exercice 7

--- Exercice5-6/index.php
+++ Exercice5-6/index.php
@@ -9,7 +9,7 @@
 
     <?php 
     
-    if (isset($_POST['gender'], $_POST['firstName'], $_POST['lastName']) && (!empty($_POST['gender']) && !empty($_POST['firstName']) && !empty($_POST['lastName'])) ) {
+    if (isset($_POST['gender'], $_POST['firstName'], $_POST['lastName'], $_POST['myFile']) && (!empty($_POST['gender']) && !empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['myFile'])) ) {
         var_dump($_POST);
     }else {
         ?>
@@ -24,7 +24,9 @@
 
         <label for="lastName">Nom :</label>
         <input type="text" name="lastName" id="lastName">
-        
+
+        <input type="file" name="myFile"/>
+
         <input type="submit" value="submit">
     </form>
     <?php
